093935e3e12e1c371c6726f88640c76847392955
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-03-19 15:52:39 +0100

    .

---
+++ JS/myscript.js
@@ -0,0 +1,33 @@
+// Train Tickes price 
+
+ /** Tasks:
+  * How much distance will you have to cover? (kilometres)
+  * The rate is calculated at Є o,21 per kilometre
+  * -20% under 18 (<18)
+  * -40% over 65 (>=65) 
+  */
+
+
+// 1st task
+ var distance = parseInt(prompt ("What is the total distance of your journey in kilometres?") );
+ console.log(distance)
+
+ // 2nd task
+
+ //The rate is calculated at Є o,21 per kilometre
+ var price =  Є o,21
+
+ // 3rd task
+
+ var age = parseInt(prompt ("How old are you?") );
+ // console.log("I'm " + age + " years old")
+
+// 4th task
+
+if (age < 18) {
+    console.log("20% off")
+} else if {
+    (age >= 65) {
+        console.log (40% off)
+    }
+}
