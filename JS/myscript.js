33442dbc9c2e344695c455c8af31eb40986cce23
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-03-19 16:57:23 +0100

    .

--- JS/myscript.js
+++ JS/myscript.js
@@ -2,7 +2,7 @@
 
  /** Tasks:
   * How much distance will you have to cover? (kilometres)
-  * The rate is calculated at Є o,21 per kilometre
+  * The rate is calculated at Є 0,21 per kilometre
   * -20% under 18 (<18)
   * -40% over 65 (>=65) 
   */
@@ -10,12 +10,15 @@
 
 // 1st task
  var distance = parseInt(prompt ("What is the total distance of your journey in kilometres?") );
- console.log("Total distance: ", distance);
+ console.log("Your distance is: ", distance);
 
  // 2nd task
 
  //The rate is calculated at Є o,21 per kilometre
- var price =  Є o,21
+ var price = 0.21;
+
+ var totalPrice = price * distance;
+ console.log(totalPrice);
 
  // 3rd task
 
@@ -25,9 +28,17 @@
 // 4th task
 
 if (age < 18) {
-    console.log("20% off")
-} else if {
-    (age >= 65) {
-        console.log (40% off)
-    }
+     console.log("20% off: ", amountDue);
+    var discount = totalPrice * 20 / 100;
+    var amountDue = totalPrice - discount;
+} else if (age >= 65) {
+     console.log ("40% off: ", amountDue);
+    var discount = totalPrice * 40 / 100;
+    var amountDue = totalPrice - discount;
+} else {
+     console.log ("0% off: ", amountDue);
+    var amountDue = totalPrice;
 }
+     
+        
+    
